d21b0ac62848a9ab78c025b5c91d2018efc1aaac
jacpimbi <jacksonsylas59@gmail.com>
2022-04-21 04:52:50 +0430

    making of models

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
-const userRoute = require("./routes/user");
+const userRoute = require("./router/user");
 
 dotenv.config();
 
@@ -13,6 +13,8 @@ mongoose
       console.log(err);
   });
 
+app.use(express.json());
+app.use("/api/users", userRoute);
 
 app.listen(process.env.PORT || 5000, ()=>{ 
     console.log("Backend server is running!");
