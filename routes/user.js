4581fb4f515684fa286299ebb2a268894bf7df74
jacpimbi <jacksonsylas59@gmail.com>
2022-04-28 04:45:36 +0430

    testing APIs

--- routes/user.js
+++ routes/user.js
@@ -1,5 +1,5 @@
 const User = require("../models/User");
-const { verifyToken, verifyTokenAndAuthorization } = require("./verifyToken");
+const { verifyToken, verifyTokenAndAuthorization, verifyTokenAndAdmin } = require("./verifyToken");
 
 const express = require('express');
 const router = express.Router();
@@ -15,17 +15,52 @@ router.put("/:id",verifyTokenAndAuthorization, async(req, res)=>{
     }
 
     try{
-        const updateUser = await User.findByIdAndUpdate(
+        const updatedUser = await User.findByIdAndUpdate(
             req.params.id,
-             {
+            {
             $set: req.body
-        },
+            },
            {new: true}
            );
-           res.status(200).json(updatedUser);
+           res.status(200).json(updatedUser);    
     }catch(err) {
         res.status(500).json(err);
     }
 });
+
+//DELETE
+router.delete("/:id",verifyTokenAndAuthorization, async(req, res)=>{
+    try{
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted....");
+    }catch(err) {
+     res.status(500).json(err);
+    }
+});
+
+//GET USER
+router.get("/find/:id", verifyTokenAndAdmin, async(req, res)=>{
+    try{
+       const user =  await User.findById(req.params.id)
+       const { password, ...others } = user._doc;  
+       res.status(200).json({others});
+    }catch(err) {
+     res.status(500).json(err);
+    }
+});
+
+//GET ALL USER
+router.get("/", verifyTokenAndAdmin, async(req, res)=>{
+    const query = req.query.new
+    try{
+       const users = query
+        ? await User.find().sort({_id:-1}).limit(5)
+        : await User.find();
+       res.status(200).json({users});
+    }catch(err) {
+     res.status(500).json(err);
+    }
+});
+
 
 module.exports = router;
