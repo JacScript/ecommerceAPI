17539ab4d8a03511e745ee056c87537a879fbaff
jacpimbi <jacksonsylas59@gmail.com>
2022-05-04 02:59:35 +0430

    ammends

--- routes/cart.js
+++ routes/cart.js
@@ -58,7 +58,7 @@ router.get("/find/:userId",verifyTokenAndAuthorization, async(req, res)=>{
 router.get("/", verifyTokenAndAdmin, async(req, res)=>{
      try{
            const carts = await Cart.find();  
-           res.status.apply(200).json(carts);
+           res.status(200).json(carts);
      }catch(err) {
          res.status(500).json(err);
      }
